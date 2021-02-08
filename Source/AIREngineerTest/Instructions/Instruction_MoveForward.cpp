d7bbb0700d829230021a93b832d95d9bb0f3aed2
Joel <Disturbed_4u2001@yahoo.com>
2021-02-08 09:24:54 -0500

    Run away!
    Added a simple turn away if there is a collision in our move.

--- Source/AIREngineerTest/Instructions/Instruction_MoveForward.cpp
+++ Source/AIREngineerTest/Instructions/Instruction_MoveForward.cpp
@@ -20,7 +20,18 @@ void UInstruction_MoveForward::ExecuteInstruction(ABaseRobot* robot)
         auto forwardLocation = curLocation + robot->GetActorForwardVector() * distanceToMove;
 
         auto newLocation = FMath::VInterpTo(robot->GetActorLocation(), forwardLocation, 1, 1);
-        robot->SetActorRelativeLocation(newLocation, true);
+
+        FHitResult OutHit;
+        robot->SetActorRelativeLocation(newLocation, true, &OutHit, ETeleportType::None);
+
+        //check the resullt to see if we have collided
+        if (OutHit.bBlockingHit)
+        {
+            auto curRot = robot->GetActorRotation();
+
+            //hit something, Spin me right round baby!
+            robot->SetActorRotation(FRotator(curRot.Pitch, curRot.Yaw + 180, curRot.Roll) );
+        }
 
         isComplete = true;
 
